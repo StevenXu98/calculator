4ceaf41d701f9869e2c7e0304dba051c9ce0ba52
StevenXu <steven_xu98@outlook.com>
2016-11-29 14:46:37 +0800

    fix the bug

--- calculator.c
+++ calculator.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <float.h>
 #include <math.h>
 
 
@@ -97,9 +98,7 @@ void getexpression()
 	 p = expression;
 	 while((*p = getchar()) != '\n'){
 	 	if(*p != ' '){
-	 		if((*p >= 'A')&&(*p <= 'Z')){
-	 			*p += 'a'-'A';//将大写转化为小写
-	 		}
+	 		if(isupper(*p)) *p = tolower(*p);//将大写转化为小写
 	 	p++;
 	 	limit++;
 	 	}
@@ -114,7 +113,7 @@ int judge(char ch){
 	else if((ch == '+')&&(ch == '-')){
 		if((i == 0)||(expression[i-1] == '('))//若+/-在字符串头或'('后则为正负号，作为一个有效字符读入
 			return 1;
-	}
+	}//else if((ch == '=')) exit(0);
 	return 0;
 }
 
@@ -128,24 +127,25 @@ void getOneChar()//用'\0'分割字符串
 		OneChar.kind = 0;	
 		return;
 	}
-	else if((*p >= 'a')&&(*p <= 'z')){
+	if((*p >= 'a')&&(*p <= 'z')){
 		while((expression[i+1] >= 'a')&&(expression[i+1] <= 'z'))
 			*++p = expression[++i];
 	}
-	
 	i++;
 	*++p = '\0';
-//	if(!strcmp(OneChar.str,"pi")){
-//		strcpy(OneChar.str,"3.1415926535897932");
-////		sprintf(OneChar.str,"%.16g",acos(-1.0));
-//		OneChar.kind = 0;
-//		return;
-//	}
-//	else if(!strcmp(OneChar.str,"e")){
-//		sprintf(OneChar.str,"%.16g",exp(1));
-//		OneChar.kind = 0;
-//		return;
-//	}
+	
+		
+	if(!strcmp(OneChar.str,"pi")){
+//		strcpy(OneChar.str,"3.14");
+		sprintf(OneChar.str,"%.16g",acos(-1.0));
+		OneChar.kind = 0;
+		return;
+	}
+	else if(!strcmp(OneChar.str,"e")){
+		sprintf(OneChar.str,"%.16g",exp(1));
+		OneChar.kind = 0;
+		return;
+	}
 	OneChar.kind = 1;
 	return;
 }//从表达式中截取一个有效的字符串
@@ -162,7 +162,7 @@ char Prioritize(ElemenType *operator1, ElemenType *operator2)
 	}
 	else if(!strcmp(oper1,"!")){
 		operator1->type = 1;
-		return '>';//
+		return '>';
 	}
 	else if(!strcmp(oper1,"+")||!strcmp(oper1,"-")||!strcmp(oper1,"*")||!strcmp(oper1,"/")||
 			!strcmp(oper1,"(")||!strcmp(oper1,")")||!strcmp(oper1,"^")||!strcmp(oper1,"=")){
@@ -170,32 +170,33 @@ char Prioritize(ElemenType *operator1, ElemenType *operator2)
 		switch(oper1[0]){
 			case '+': case '-': return (!strcmp(oper2,"+")||!strcmp(oper2,"-")||
 										!strcmp(oper2,")")||!strcmp(oper2,"=")) ? '>' : '<';
-			case '*': case '/': return (strcmp(oper2,"(")||strcmp(oper2,"^")) ? '>' : '<';
-			case '(': return !strcmp(oper2,")") ? '=' : '<';
+			case '*': case '/': return (!strcmp(oper2,"+")||!strcmp(oper2,"-")||!strcmp(oper2,"*")||
+										!strcmp(oper2,"/")||!strcmp(oper2,")")||!strcmp(oper2,"=")) ? '>' : '<';
+			case '(': return !strcmp(oper2,")") ? '#' : '<';
 			case ')': return '>';
-			case '^': return (strcmp(oper2,"(")||strcmp(oper2,"!")||strcmp(oper2,"^")) ? '>' : '<';
-			case '=': return !strcmp(oper2,"=") ? '=' : '<';
+			case '^': return (!strcmp(oper2,"(")||!strcmp(oper2,"!")||!strcmp(oper2,"^")) ? '<' : '>';
+			case '=': return !strcmp(oper2,"=") ? '#' : '<'; 
 		}
 	}else {printf("ERROR:102");exit(0);}//出现不可识别的运算符
 }
 //优先级返回结束
 
-int factorial(n)
+int factorial(int n)
 {
 	if(n <= 1) return 1;
 	else return n * factorial(n-1);
-}//阶乘运算 未测试
+}//阶乘运算 
 
 ElemenType Calculate(ElemenType num1, ElemenType oper, ElemenType num2)
 {
 	ElemenType result;
 	if(oper.type == 1){
-		if(!strcmp(oper.operator,"!")) result.number = factorial(num1.number);
-		else if(!strcmp(oper.operator,"lg")) result.number = log10(num1.number);
-		else if(!strcmp(oper.operator,"ln")) result.number = log(num1.number);
-		else if(!strcmp(oper.operator,"sin")) result.number = sin(num1.number);
-		else if(!strcmp(oper.operator,"cos")) result.number = cos(num1.number);
-		else if(!strcmp(oper.operator,"tan")) result.number = tan(num1.number);
+		if(!strcmp(oper.operator,"!")) result.number = factorial(num2.number);
+		else if(!strcmp(oper.operator,"lg")) result.number = log10(num2.number);
+		else if(!strcmp(oper.operator,"ln")) result.number = log(num2.number);
+		else if(!strcmp(oper.operator,"sin")) result.number = sin(num2.number);
+		else if(!strcmp(oper.operator,"cos")) result.number = cos(num2.number);
+		else if(!strcmp(oper.operator,"tan")) result.number = tan(num2.number);
 	}else{
 		switch(*oper.operator) {
 			case '+': result.number = num1.number + num2.number; break;
@@ -215,15 +216,16 @@ int main()
 	int mode;
 	char s[1024];
 
-	printf("简易计算器v0.1.0 \n\n");
+	printf("简易计算器v0.2.0 \n\n");
 	printf("支持的运算符:\n| + | - | * | / | \n|sin|cos|tan|   | \n| lg| ln| ^ | ! |\n\n");
+	printf("pi为圆周率 e为欧拉数\n");
 	printf("错误代码:\n101: 表达式不合法，请检查表达式输入是否正确 \n102: 有违法运算符\n\n");
 	printf("输入2进入文件读取模式\n");
 	scanf("%d",&mode);
 	fflush(stdin);
-	if(mode == 2) freopen("input","r",stdin);
-//		printf("输入表达式，回车求值 \n");
-//		printf("输入quit结束程序\n\n\n");
+	if(mode == 2) freopen("input","r",stdin);//重新定义输入流，实现文件操作
+		printf("\n输入表达式，回车求值 \n");
+		printf("输入quit结束程序\n\n\n");
 		
 	while(1){
 		i = 0;
@@ -238,13 +240,13 @@ int main()
 		while(strcmp(OneChar.str,"=")||strcmp(OPER->next->date.operator,"=")){
 			if(OneChar.kind){
 				strcpy(oper.operator,OneChar.str);
-				switch(Prioritize(&(OPER->next->date), &oper)){
+				switch(Prioritize(&(OPER->next->date),&oper)){
 					case '<':
 						strcpy(oper.operator,OneChar.str);
 						Push(oper,OPER);
 						getOneChar();
 						break;
-					case '=':
+					case '#':
 						Pop(OPER,&oper);
 						getOneChar();
 						break;
@@ -261,9 +263,15 @@ int main()
 				getOneChar();
 			}
 		}
-		if(mode == 2) printf("%s",expression);
-		printf("%.16g\n\n",NUM->next->date.number);
+		
+		if(strcmp(expression,"=")){
+			printf("%s",expression);
+//			if(_isnan(NUM->next->date.number)) putchar(236);
+			printf("%.16g\n\n",NUM->next->date.number);
+		}
+		
 		free(expression);
+
 		DestroyStack(&NUM);
 		DestroyStack(&OPER);
 	}
